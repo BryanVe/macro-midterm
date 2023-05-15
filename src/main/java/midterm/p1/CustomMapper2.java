197fe8ed9e5d6dc087fa1c41074b8d695b936e1c
AnthonyLzq <sluzquinosa@uni.pe>
2023-05-15 17:55:35 -0500

    feat: removed unnecessary imports

--- src/main/java/midterm/p1/CustomMapper2.java
+++ src/main/java/midterm/p1/CustomMapper2.java
@@ -1,6 +1,5 @@
 package midterm.p1;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
--- src/main/java/midterm/p1/CustomReducer2.java
+++ src/main/java/midterm/p1/CustomReducer2.java
@@ -1,6 +1,5 @@
 package midterm.p1;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
